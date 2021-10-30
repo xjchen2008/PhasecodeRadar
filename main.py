4f1d793bc31f74dc36a97883f1d4f97b8b2e6ef8
xjchen2008 <xjchen2008@gmail.com>
2021-10-30 08:20:32 -0400

    phi_1 = np.pi / M * (k*1000) M = 100  # k = 1 N = int(100/ k) # Base-code length; This determines the range resolution;

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Xingjian Chen 2021 Oct 29
+# Check out paper "Quadratic Phase Coding for High Duty Cycle Radar Operation"
+# https://www.semanticscholar.org/paper/Quadratic-Phase-Coding-for-High-Duty-Cycle-Radar-Mead-Pazmany/3ba09ab4cc5c70833a5de163babf5a5c5e061832
 import numpy as np
 from numpy.fft import fftshift, fft
 import matplotlib.pyplot as plt
@@ -8,22 +11,19 @@ def wavetable(N, phi = 0):
     #############
     # Parameters
     #############
-    c = 3e8
-    j = 1j
-    T = N / fs  # T=N/fs#Chirp Duration
     # print (N)
-    t = np.linspace(0, T, N)
+    t = np.linspace(0, Tp-Tp/N, N) # Subpulse duration # T=N/fs
     bw = 10e6  # 20e6#20e6#45.0e5
     fc = 0  # 50e6# 50e6#0e6
     f0 = fc - bw / 2  # -10e6#40e6 # Start Freq
     f1 = fc + bw / 2  # 10e6#60e6# fs/2=1/2*N/T#End freq
     # print('f0 = ',f0/1e6, 'MHz;', 'f1=', f1/1e6, 'MHz')
-    k = bw / T  # (f1 - f0) / T
+    k = bw / Tp  # (f1 - f0) / T
     phi0 = -np.pi / 2  # Phase
 
     distance = c * freq / k / 2.0  # = c/(2BW), because need an array of distance, so use freq to represent distance.
-    win=np.hamming(N)
-    # win=np.blackman(N)
+    #win=np.hamming(N)
+    win=np.blackman(N)
     #win = 1
     ##################
     # Create the chirp
@@ -37,7 +37,7 @@ def wavetable(N, phi = 0):
     y_s = np.sin(1*2*np.pi*fs/N*t+ phi)#+ np.sin(4*np.pi*fs/N*t)# just use LO to generate a LO. The
     yq_s = np.sin(1*2*np.pi*fs/N*t-np.pi/2 + phi)# + np.sin(4*np.pi*fs/N*t-np.pi/2)
     y_cx_sine1 = y_s + j * yq_s
-    fo = 50e6
+    fo = 10e6
     y_s2 = np.sin(1*2*np.pi*fo*t)#+ np.sin(4*np.pi*fs/N*t)# just use LO to generate a LO. The
     yq_s2 = np.sin(1*2*np.pi*fo*t-np.pi/2)# + np.sin(4*np.pi*fs/N*t-np.pi/2)
     y_cx_sine2 = y_s2 + j * yq_s2
@@ -51,17 +51,30 @@ def phasecode(M):
     else:
         L0 = 2 * M
     n = np.linspace(0, L0-1, L0)
-    phi_1 = np.pi / M  * (k) #+ np.exp(1)/10000
+    phi_1 = np.pi / M * (k*1000) #+ np.exp(1)/10000
     phi_n =  phi_1 * np.power(n, 2)
     return phi_n
 
 
 if __name__ == '__main__':
+    c = 3e8
+    j = 1j
     M = 100  #
-    k = 25
-    N = int(100 / k) # Base-code length; This determines the range resolution;
+    k = 1
+    N = int(100/ k) # Base-code length; This determines the range resolution;
     fs = 56e6  # 250e6 #56e6 #1000e6 #250e6  # Sampling freq
+    Tp = N / fs  #Sub-pulse Duration
+    Tp0 = M*Tp # Tp0 is the full pulse duration; Same as Fp = M* Fp0 in paper(2)
     freq = np.fft.fftfreq(N*M, d=1. / fs)
+    del_d = 0.5* c * Tp # Echo trip spacing = range resolution; Sub-pulse duration determines range resolution
+    R0 = 0
+    del_Rg = del_d * 2
+
+    #Rs = R0 + i * R_del
+    #R_mi = Rs + m * del_d
+    Rmax = c * Tp0 /2 # Eqn(1) in the paper
+    distance = np.linspace(-0.5*Rmax, 0.5*Rmax, M*N) #M = Ng?
+
     x = []
     for m in range(0,M):
         phi = phasecode(M)
@@ -69,29 +82,36 @@ if __name__ == '__main__':
         #x = np.concatenate((x, wavetable(N=N, phi=0)))
     #x = wavetable(N = M*N, phi = 0)
 
-    win_all = np.ones(M*N)
+    win_all = 1
     #win_all = np.blackman(M*N)
     x_win = np.multiply(x, win_all)
-    pc = fn.PulseCompr(x_win, np.roll(x_win, -10), win = 1, unit='linear')
+    pc = fn.PulseCompr(x_win, np.roll(x_win, -22), win = 1, unit='linear')
 
     #######
     # Plot
     #######
+
     fn.plot_freq_db(freq, x_win, normalize=False, domain='time')
     plt.title('FFT of Phase-coded Signal')
-
-
-
     plt.figure()
     plt.plot(x_win.real,'*-')
-    '''
+
     plt.title('Phase-coded Signal')
     plt.figure()
     plt.plot(phi, '*-')
     plt.title('Initial Phase')
-    '''
+
     plt.figure()
     fn.plot_freq_db(freq, pc, normalize= True, domain='freq')
     plt.title('Pulse Compression')
+
+    #plt.xlim([-10, 10])
     #plt.ylim([-100, 100])
+    
+    plt.figure()
+    plt.plot((distance), fftshift(20*np.log10(abs(pc))),'k*-')
+    plt.xlabel('Distance [m]')
+    plt.ylabel('Magnitude [dB]')
+    plt.grid()
+    plt.xlim([-200, 200])
     plt.show()
