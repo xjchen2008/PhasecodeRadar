35a17fc19ec5d00a2ded5298d3de8d3f7d71eb5e
xjchen2008 <xjchen2008@gmail.com>
2021-11-01 23:37:11 -0400

    tune for the parameters v1

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def wavetable(N, phi = 0):
 
     distance = c * freq / k / 2.0  # = c/(2BW), because need an array of distance, so use freq to represent distance.
     #win=np.hamming(N)
-    win=np.blackman(N)
+    win=np.power(np.blackman(N), 1)
     #win = 1
     ##################
     # Create the chirp
@@ -50,13 +50,39 @@ def phasecode(M):
         L0 = M
     else:
         L0 = 2 * M
-    n = np.linspace(0, L0-1, L0)
-    phi_1 = np.pi / M * (k*1000) #+ np.exp(1)/10000
-    phi_n =  phi_1 * np.power(n, 2)
-    return phi_n
+    #n = np.linspace(0, L0-1, L0)
+    phi_1 = np.pi / M * (k0)#(k*1000) #+ np.exp(1)/10000
+    phi = np.zeros(L0)
+    for n in range(0, L0, 1):
+        #phi[n] = phi_1 * np.power(n, 2)
+        if n<L0/2:
+            phi[n] =  phi_1 * np.power(n, 2)
+        if n >= L0/2:
+            phi[n] = phi_1 * np.power(L0 -n, 2)
+    '''
+    n = np.linspace(0, L0 - 1, L0)
+    phi_n = phi_1 * np.power(n, 2)
+    '''
+    return phi
 
 
 if __name__ == '__main__':
+    c = 3e8
+    j = 1j
+    M = 10
+    Fp0 = 200e3 # PRF Related to range resolution and range gate. full phase-coded signal is 1ms duration as FMCW SDR radar
+    Fp = M * Fp0
+    Tp0 = 1 / Fp0
+    Tp = 1 / Fp
+    k0 = 1 # ralated to freq response
+    fs = 60e6
+    N = int(Tp * fs)
+    # N = 60
+    #fs = N / Tp
+    d_fs = 1/fs *c /2 # distance of spacing for each sample in time domain
+    print('d_fs=',d_fs, '\nfs=', fs/1e6,'MHz', '\nN=',N,
+          '\nRange_resolution=',N * d_fs)
+    '''
     c = 3e8
     j = 1j
     M = 100  #
@@ -65,15 +91,16 @@ if __name__ == '__main__':
     fs = 56e6  # 250e6 #56e6 #1000e6 #250e6  # Sampling freq
     Tp = N / fs  #Sub-pulse Duration
     Tp0 = M*Tp # Tp0 is the full pulse duration; Same as Fp = M* Fp0 in paper(2)
+    
+    '''
     freq = np.fft.fftfreq(N*M, d=1. / fs)
     del_d = 0.5* c * Tp # Echo trip spacing = range resolution; Sub-pulse duration determines range resolution
     R0 = 0
     del_Rg = del_d * 2
-
     #Rs = R0 + i * R_del
     #R_mi = Rs + m * del_d
     Rmax = c * Tp0 /2 # Eqn(1) in the paper
-    distance = np.linspace(-0.5*Rmax, 0.5*Rmax, M*N) #M = Ng?
+    distance = c/2* freq/(M*np.power(Fp0, 2))/k0#np.linspace(-0.5*Rmax, 0.5*Rmax, M*N) #M = Ng?
 
     x = []
     for m in range(0,M):
@@ -85,7 +112,7 @@ if __name__ == '__main__':
     win_all = 1
     #win_all = np.blackman(M*N)
     x_win = np.multiply(x, win_all)
-    pc = fn.PulseCompr(x_win, np.roll(x_win, -22), win = 1, unit='linear')
+    pc = fn.PulseCompr(rx = np.roll(x_win, 0), tx = x_win, win = 1, unit='linear')
 
     #######
     # Plot
@@ -109,9 +136,9 @@ if __name__ == '__main__':
     #plt.ylim([-100, 100])
     
     plt.figure()
-    plt.plot((distance), fftshift(20*np.log10(abs(pc))),'k*-')
+    plt.plot(fftshift(distance), fftshift(20*np.log10(abs(pc))),'k*-')
     plt.xlabel('Distance [m]')
     plt.ylabel('Magnitude [dB]')
     plt.grid()
-    plt.xlim([-200, 200])
+    plt.xlim([-1000, 1000])
     plt.show()
