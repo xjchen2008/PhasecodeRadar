66b0790db1c9e764592c21b2476b6f0b71aea876
xjchen2008 <xjchen2008@gmail.com>
2021-11-17 13:36:22 -0500

    p3 code with freq spacing between adjacent spectral lines

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def wavetable(N, phi = 0):
     #############
     # print (N)
     #t = np.linspace(0, Tp-Tp/N, N) # Subpulse duration # T=N/fs # No the last point
-    t = np.linspace(0, Tp , N)  # With the last point
-
+    t = np.linspace(0, Tp, N)  # With the last point
+    n = np.linspace(0, N-1, N)
     fc = 0  # 50e6# 50e6#0e6
     f0 = fc - bw / 2  # -10e6#40e6 # Start Freq
     f1 = fc + bw / 2  # 10e6#60e6# fs/2=1/2*N/T#End freq
@@ -26,7 +26,7 @@ def wavetable(N, phi = 0):
     distance = c * freq / k / 2.0  # = c/(2BW), because need an array of distance, so use freq to represent distance.
     #win=np.hamming(N)
     #win=np.power(np.blackman(N), 2)
-    win = np.power(np.hamming(N), 1)
+    win = np.power(np.hanning(N), 1)
     #win = 1
     ##################
     # Create the chirp
@@ -44,8 +44,13 @@ def wavetable(N, phi = 0):
     y_s2 = np.sin(1*2*np.pi*fo*t)#+ np.sin(4*np.pi*fs/N*t)# just use LO to generate a LO. The
     yq_s2 = np.sin(1*2*np.pi*fo*t-np.pi/2)# + np.sin(4*np.pi*fs/N*t-np.pi/2)
     y_cx_sine2 = y_s2 + j * yq_s2
-
-    return np.multiply(y_cx_sine1, win)
+    y_s3 = np.sin(np.pi/N*np.power(n,2))  # + np.sin(4*np.pi*fs/N*t)# just use LO to generate a LO. The
+    yq_s3 = np.sin(np.pi/N*np.power(n,2)-np.pi/2)  # + np.sin(4*np.pi*fs/N*t-np.pi/2)
+    y_cx_sine3 = y_s3 + j * yq_s3
+    y_p4 = np.sin(k0*np.pi/N*n*(n-N))  # + np.sin(4*np.pi*fs/N*t)# just use LO to generate a LO. k0 is related to the cycle/ period of the phase coding signal
+    yq_p4 = np.sin(k0*np.pi/N*n*(n-N)-np.pi/2)  # + np.sin(4*np.pi*fs/N*t-np.pi/2)
+    y_cx_sine4 = y_p4 + j * yq_p4
+    return np.multiply(y_cx_sine4, win)
 
 
 def phasecode(M):
@@ -56,7 +61,7 @@ def phasecode(M):
     #n = np.linspace(0, L0-1, L0)
     phi_1 = np.pi / M * (k0)#(k*1000) #+ np.exp(1)/10000
     phi = np.zeros(L0)
-    '''
+
     for n in range(0, L0, 1):
         #phi[n] = phi_1 * np.power(n, 2)
         if n<L0/4:
@@ -67,16 +72,22 @@ def phasecode(M):
             phi[n] =  phi_1 * np.power(n-L0/2, 2)
         if n >= L0 *3/ 4:
             phi[n] = phi_1 * np.power(L0 -n, 2)
-    '''
 
+    '''
     # Previous initial phase
     for n in range(0, L0, 1):
         #phi[n] = phi_1 * np.power(n, 2)
+        # paper order
         if n<L0/2:
             phi[n] =  phi_1 * np.power(n, 2)
         if n >= L0/2:
-            phi[n] = phi_1 * np.power(L0 -n, 2)
-
+            phi[n] = phi_1 * np.power(L0-n, 2)
+        # chirp order
+        #if n<L0/2:
+        #    phi[n] =  phi_1 * np.power(L0/2-n, 2)
+        #if n >= L0/2:
+        #    phi[n] = phi_1 * np.power(n-L0/2, 2)
+    '''
     '''
     n = np.linspace(0, L0 - 1, L0)
     phi_n = phi_1 * np.power(n, 2)
@@ -84,19 +95,29 @@ def phasecode(M):
     #phi = np.zeros(L0)
     return phi
 
+def test():
+    fn.plot_freq_db(freq, x_win, normalize=False, domain='time')
+    # plt.plot(fftshift(freq), fftshift(20*np.log10(abs(np.fft.fft(x_win,axis=0)))))
+    plt.title('FFT of Phase-coded Signal')
+    plt.figure()
+    plt.plot(x_win.real, '*-')
 
+
+    
+    
 if __name__ == '__main__':
     c = 3e8
     j = 1j
     bw = 56e6  #FMCW chirp bandwidth 20e6#20e6#45.0e5
-    M = 100 # tune with Fp0 to increase range gate or range ambiguity
-    Fp0 = 20*16e3/1 # PRF Related to range resolution and range gate. full phase-coded signal is 1ms duration as FMCW SDR radar
+    a = 2
+    M = 25 # tune with Fp0 to increase range gate or range ambiguity
+    Fp0 = 8e3 * a # PRF Related to range resolution and range gate. full phase-coded signal is 1ms duration as FMCW SDR radar
     Fp = M * Fp0
     Tp0 = 1 / Fp0
     Tp = 1 / Fp
     k0 = 1# ralated to freq response
-    fs = 56e6*1
-    N = int(Tp * fs)
+    fs = 20e6
+    N = int(Tp * fs) #20
     '''N = 60
     fs = N / Tp'''
     d_fs = 1/fs *c /2 # The "unit resolution" from unit sample time; distance of spacing for each sample in time domain
@@ -137,10 +158,12 @@ if __name__ == '__main__':
     win_all = 1
     #win_all = np.blackman(M*N)
     x_win = np.multiply(x, win_all)
+    #test()
+
 
     #
-    x_win_BPF_real = dsp_filters_BPF.run(x_win.real,fs=fs, highcut=15e6, lowcut=5e6)
-    x_win_BPF = hilbert(x_win_BPF_real)
+    #x_win_BPF_real = dsp_filters_BPF.run(x_win.real,fs=fs, highcut=15e6, lowcut=5e6)
+    #x_win_BPF = hilbert(x_win_BPF_real)
     #x_win = x_win_BPF
     pc = fn.PulseCompr(rx = np.roll(x_win, 2), tx = x_win, win = 1, unit='linear')
 
@@ -154,16 +177,15 @@ if __name__ == '__main__':
     plt.figure()
     plt.plot(x_win.real,'*-')
 
-
-
-
-
+    
+    
+    
 
     plt.title('Phase-coded Signal')
     plt.figure()
     plt.plot(phi, '*-')
     plt.title('Initial Phase')
-
+    
     plt.figure()
     fn.plot_freq_db(freq, pc, normalize= True, domain='freq')
     plt.title('Pulse Compression')
@@ -177,7 +199,8 @@ if __name__ == '__main__':
     plt.xlabel('Distance [m]')
     plt.ylabel('Magnitude [dB]')
     plt.grid()
-    #plt.xlim([-1000, 1000])
+    plt.xlim([-100, 1000])
+
     '''
     plt.figure()
     del_F= np.linspace(0,100e6, 100) # Fourier transform frequency resoltuion
@@ -188,3 +211,4 @@ if __name__ == '__main__':
     plt.xlabel('Range Resolutijon [m]')
     '''
     plt.show()
+    np.save(file='waveform_phase_code', arr=x_win)
